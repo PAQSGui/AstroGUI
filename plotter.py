ff7643fde7cafe6ba27d666cdadd351b77e20832
Sofus Kjær <sofkj@itu.dk>
2025-02-14 10:05:26 +0100

    add changes to plotter

--- plotter.py
+++ plotter.py
@@ -1,9 +1,16 @@
 from astropy.io import fits
 import Spec_tools as tool
+import os
+import matplotlib.pyplot as plt
 
-with fits.open("spec-1222-52763-0091.fits") as hdul:
+with fits.open("spec-1222-52763-0092.fits") as hdul:
     hdul.info()
 
+my_file = tool.SDSS_spectrum("spec-1222-52763-0091.fits")
+
+os.environ["XDG_SESSION_TYPE"] = "xcb"
+my_file.Plot()
+plt.show()
 
 
 
