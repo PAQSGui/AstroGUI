f46fa655dcf6cc744c8f4cab51569fb3d0e253ed
Torben <toen@itu.dk>
2025-02-17 11:31:15 +0100

    File name and answer is now saved to csv

--- app.py
+++ app.py
@@ -11,6 +11,9 @@ def NavBtn (msg, delta):
     #Tests: Can you go out of bounds? Is the selected file a FITS? Is it the correct format of FITS?
     print("Button clicked: " + msg)
     global cursor
+    with open("data.csv", "a") as f:
+        # Replace 'files[cursor]' with the target name once we can extract that information
+        f.write(f"{files[cursor]}, {msg}\n")
     cursor=cursor+delta
     print("Current File: " + files[cursor])
 
@@ -34,8 +37,8 @@ def OpenFolder():
     os.environ["XDG_SESSION_TYPE"] = "xcb" #is this still used?
     my_file.Plot()
     fig=plt.gcf()#get current figure
-    canvas=tkplot.FigureCanvasTkAgg(fig,spectrum_frame)
-    canvas.get_tk_widget().pack(padx=5, pady=5,side=tk.TOP)
+    #canvas=tkplot.FigureCanvasTkAgg(fig,spectrum_frame)
+    #canvas.get_tk_widget().pack(padx=5, pady=5,side=tk.TOP)
 
 
 root = tk.Tk()
@@ -52,24 +55,24 @@ tk.Label(data_frame, text="Target metadata:\nMAG, MAG_TYPE, target name,\nE(B-V)
 
 vis_frame = tk.Frame(root, bg="skyblue")
 vis_frame.pack(padx=5, pady=5, side=tk.TOP, fill=tk.BOTH, expand=True)
-spectrum_frame = tk.Frame(vis_frame, bg="orange")
-spectrum_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+#spectrum_frame = tk.Frame(vis_frame, bg="orange")
+#spectrum_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 #spectrum=tk.Label(spectrum_frame, text="L1J spectrum\nL2XP: best-fit template + Z_BEST (plus lines)\nL2CP: CLASS, PROB, CLASS2, PROB2, ...\nOverplot sky spectrum\nOverplot telluric abs.")
 #spectrum.pack(padx=5, pady=5,side=tk.TOP)
 
-colors_frame = tk.Frame(spectrum_frame, bg="skyblue")
-colors_frame.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
-red_frame = tk.Frame(colors_frame, bg="red")
-red_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-tk.Label(red_frame, text="L1R").place(relx=0.5, rely=0.5, anchor='center')
-green_frame = tk.Frame(colors_frame, bg="green")
-green_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-tk.Label(green_frame, text="L1G").place(relx=0.5, rely=0.5, anchor='center')
-blue_frame = tk.Frame(colors_frame, bg="blue")
-blue_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
-tk.Label(blue_frame, text="L1B").place(relx=0.5, rely=0.5, anchor='center')
+#colors_frame = tk.Frame(spectrum_frame, bg="skyblue")
+#colors_frame.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+#red_frame = tk.Frame(colors_frame, bg="red")
+#red_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+#tk.Label(red_frame, text="L1R").place(relx=0.5, rely=0.5, anchor='center')
+#green_frame = tk.Frame(colors_frame, bg="green")
+#green_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+#tk.Label(green_frame, text="L1G").place(relx=0.5, rely=0.5, anchor='center')
+#blue_frame = tk.Frame(colors_frame, bg="blue")
+#blue_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
+#tk.Label(blue_frame, text="L1B").place(relx=0.5, rely=0.5, anchor='center')
 
-redshift_slide = tk.Button(spectrum_frame, text="Redshift slide, template drop-down").pack(padx=5, pady=5,side=tk.BOTTOM)
+#redshift_slide = tk.Button(spectrum_frame, text="Redshift slide, template drop-down").pack(padx=5, pady=5,side=tk.BOTTOM)
 
 spectrum_buttons = tk.Frame(vis_frame, bg="yellow")
 spectrum_buttons.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
