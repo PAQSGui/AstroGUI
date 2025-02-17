107c791b9e6f73094972958b1dad8c1f8224c13e
Torben <toen@itu.dk>
2025-02-17 09:54:53 +0100

    Added in extra meta data getters, in case we find the data

--- file_handling.py
+++ file_handling.py
@@ -15,5 +15,12 @@ for k in hdul[0].header:
 
 def getTargetData():
     name = hdul[0].header['NAME']
+    
+    # WHY IS THERE NO EBV CARD?
+    #ebv = hdul[0].header['EBV']
+
+    # WHERE IS THE MAGNITUDE? :(
+    #mag = hdul[0].header['*MAG*']
+
 
     return name
