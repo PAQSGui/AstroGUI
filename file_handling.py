6a7b25bf50fbe05ce199c375b5b1b03701ab7ace
Torben <toen@itu.dk>
2025-02-17 09:58:14 +0100

    Clean up

--- file_handling.py
+++ file_handling.py
@@ -2,13 +2,6 @@ from astropy.io import fits
 
 hdul = fits.open("data/spec-2237-53828-0531.fits", ignore_missing_simple=True)
 
-# data = hdul[0].data
-# header = hdul[0].header
-# print(data)
-# print(header)
-#print(hdul.info())
-#print(hdul[0].header['NAME'])
-
 for k in hdul[0].header:
     print(k)
 
