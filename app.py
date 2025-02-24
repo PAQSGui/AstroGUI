528a1b4d2bb6e10416e04e421ac318b42f54f875
Sofus Kjær <sofkj@itu.dk>
2025-02-24 11:00:15 +0100

    Tidied up UI-elements in app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import tkinter as tk
 import Spec_tools as tool
 import os
-import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.backends.backend_tkagg as tkplot
 
@@ -13,7 +12,6 @@ from urllib.request import urlretrieve
 from IPython.display import Image
 
 from tkinter import filedialog as browse
-from matplotlib.figure import Figure
 from plotter import PlotFile
 
 global directory
@@ -48,6 +46,7 @@ def OpenFolder():
 def LoadFile(delta=1):
     global cursor
     print("Current File: " + files[cursor])
+
     while True:
         try:
             my_file = tool.SDSS_spectrum(directory+"/"+files[cursor]) #not OS safe I think
@@ -59,9 +58,6 @@ def LoadFile(delta=1):
                 cursor = cursor-1
             print("skipping bad file\n")
             continue
-
-    #generate 4 steps in visible spectrum
-    visrange = np.linspace(3800,7500,4)
 
     PlotFile(my_file)
     canvas.draw() 
@@ -96,25 +92,25 @@ def LoadPicture():
     urlretrieve(url, image_name)
     Image(image_name) #load the image into the notebook
 
-root = tk.Tk()
 
+# The UI elements
+root = tk.Tk()
 root.minsize(400, 400)
+
 # Create two labels
 data_frame = tk.Frame(root, bg="red")
-data_frame.pack(padx = 5, pady = 5, side = tk.TOP, fill = tk.BOTH, expand = True)
+data_frame.pack(padx = 5, pady = 5, side = tk.TOP, fill = tk.BOTH)
 
+# Header with info about the file
 tk.Label(data_frame, text = "What is the DELTA-MAG of ±2 neighbors on the CCD").pack(padx = 5, pady = 5, side = tk.LEFT)
+tk.Label(data_frame, text = "Target metadata:\nMAG, MAG_TYPE, target name,\nE(B-V)_gal").pack(padx = 5, pady = 5,side = tk.RIGHT)
 
-# display info from the header in this button
-tk.Label(data_frame, text="Target metadata:\nMAG, MAG_TYPE, target name,\nE(B-V)_gal").pack(padx = 5, pady = 5,side = tk.RIGHT)
-
+# The graphs
 vis_frame = tk.Frame(root, bg="skyblue")
 vis_frame.pack(padx = 5, pady = 5, side = tk.TOP, fill = tk.BOTH, expand = True)
 spectrum_frame = tk.Frame(vis_frame, bg = "orange")
 spectrum_frame.pack(side = tk.LEFT, fill = tk.BOTH, expand = True)
 canvas = CreateCanvas(bigFig, spectrum_frame)
-#spectrum=tk.Label(spectrum_frame, text="L1J spectrum\nL2XP: best-fit template + Z_BEST (plus lines)\nL2CP: CLASS, PROB, CLASS2, PROB2, ...\nOverplot sky spectrum\nOverplot telluric abs.")
-#spectrum.pack(padx=5, pady=5,side=tk.TOP)
 
 colors_frame = tk.Frame(spectrum_frame, bg = "skyblue")
 colors_frame.pack(side = tk.BOTTOM, fill = tk.BOTH, expand = True)
@@ -124,20 +120,21 @@ blueCanvas  = CreateCanvas(blueFig,colors_frame, tk.RIGHT)
 
 redshift_slide = tk.Button(spectrum_frame, text="Redshift slide, template drop-down").pack(padx = 5, pady = 5,side = tk.BOTTOM)
 
+# Sidepanel with buttons
 spectrum_buttons = tk.Frame(vis_frame, bg = "yellow")
-spectrum_buttons.pack(side = tk.RIGHT, fill = tk.BOTH, expand = True)
-show_stack = tk.Button(spectrum_buttons, text = "SHOW spectra of STACK").pack(padx = 5, pady = 5, side = tk.TOP, fill = tk.BOTH, expand = True)
-show_sn = tk.Button(spectrum_buttons, text = "Show S/N spec").pack(padx = 5, pady = 5, side = tk.TOP, fill = tk.BOTH, expand = True)
-show_skyview = tk.Button(spectrum_buttons, text = "Button to grab: Image cutout (DSS) 100\"x100\"", command = lambda: LoadPicture()).pack(padx=5, pady=5,side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+spectrum_buttons.pack(side = tk.RIGHT, fill = tk.BOTH)
+show_stack = tk.Button(spectrum_buttons, text = "SHOW spectra of STACK").pack(padx = 5, pady = 5, side = tk.TOP, fill = tk.BOTH)
+show_sn = tk.Button(spectrum_buttons, text = "Show S/N spec").pack(padx = 5, pady = 5, side = tk.TOP, fill = tk.BOTH)
+show_skyview = tk.Button(spectrum_buttons, text = "Button to grab: Image cutout (DSS) 100\"x100\"", command = lambda: LoadPicture()).pack(padx=5, pady=5,side=tk.BOTTOM, fill=tk.BOTH)
 
-
+# Navigation buttons in the bottom
 opts_frame = tk.Frame(root, bg="limegreen")
-opts_frame.pack(padx=5, pady=5, side=tk.BOTTOM, fill=tk.BOTH, expand=True)
-tk.Button(opts_frame, text="Go Back", command = lambda: NavBtn(msg="Go Back",delta=-1)).pack(padx=5, pady=5,side=tk.LEFT, fill=tk.BOTH, expand=True)
-tk.Button(opts_frame, text="Yes", command = lambda: NavBtn(msg="Yes",delta=1)).pack(padx=5, pady=5,side=tk.LEFT, fill=tk.BOTH, expand=True)
-tk.Button(opts_frame, text="No", command = lambda: NavBtn(msg="No",delta=1)).pack(padx=5, pady=5,side=tk.LEFT, fill=tk.BOTH, expand=True)
-tk.Button(opts_frame, text="Not sure", command = lambda: NavBtn(msg="Not sure",delta=1)).pack(padx=5, pady=5,side=tk.LEFT, fill=tk.BOTH, expand=True)
-tk.Label(opts_frame, text="NO, but why:\nWrong template; wrong redshift (4XP);\nwrong class (4CP);\nBad data (L1); Maybe sat.?").pack(padx=5, pady=5,side=tk.RIGHT, fill=tk.BOTH, expand=True)
+opts_frame.pack(padx=5, pady=5, side=tk.BOTTOM, fill=tk.BOTH)
+tk.Button(opts_frame, text="Go Back", command = lambda: NavBtn(msg="Go Back",delta=-1)).pack(padx=5, pady=5,side=tk.LEFT, fill=tk.BOTH)
+tk.Button(opts_frame, text="Yes", command = lambda: NavBtn(msg="Yes",delta=1)).pack(padx=5, pady=5,side=tk.LEFT, fill=tk.BOTH)
+tk.Button(opts_frame, text="No", command = lambda: NavBtn(msg="No",delta=1)).pack(padx=5, pady=5,side=tk.LEFT, fill=tk.BOTH)
+tk.Button(opts_frame, text="Not sure", command = lambda: NavBtn(msg="Not sure",delta=1)).pack(padx=5, pady=5,side=tk.LEFT, fill=tk.BOTH)
+tk.Label(opts_frame, text="NO, but why:\nWrong template; wrong redshift (4XP);\nwrong class (4CP);\nBad data (L1); Maybe sat.?").pack(padx=5, pady=5,side=tk.RIGHT, fill=tk.BOTH)
 
 #Menubar
 menubar = tk.Menu(root)
