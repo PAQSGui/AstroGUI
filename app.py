d5d674776ca2404cd0872a2f73269e03078909f2
Artemis <jebal@itu.dk>
2025-03-03 18:06:03 +0100

    Moved some pack functionalities into lambda functions, so changes have to be made in fewer places

--- app.py
+++ app.py
@@ -13,6 +13,15 @@ redFig = plt.figure('r')
 greenFig = plt.figure('g')
 blueFig = plt.figure('b')
 
+#pack functions
+pack0=lambda el,s: el.pack(padx=5, pady=5, side=s, fill=tk.BOTH)
+pack1=lambda el,s: el.pack(padx=5, pady=5, side=s)
+pack2=lambda el,s: el.pack(side = s, fill = tk.BOTH, expand = True)
+
+#temp ui elements
+tempLabel= lambda f,t,s: pack1(tk.Label(f, text = t),s)
+tempButton= lambda f,t: pack1(tk.Button(f, text = t),tk.TOP)
+
 def UpdateGraph(file):
     PlotFile(file)
     canvas.draw() 
@@ -20,19 +29,17 @@ def UpdateGraph(file):
     greenCanvas.draw()
     blueCanvas.draw()
 
-
-
 def CreateCanvas(fig,rootFrame, place = tk.TOP):
     canv=tkplot.FigureCanvasTkAgg(fig, rootFrame)
     canv.get_tk_widget().config(width = 200, height = 200)
-    canv.get_tk_widget().pack(padx = 5, pady = 5,side = place, fill = tk.BOTH, expand = True)
+    pack2(canv.get_tk_widget(),place)
     return canv
 
 # The UI elements
 root = tk.Tk()
 root.minsize(400, 400)
 data_frame = tk.Frame(root, bg="red")
-data_frame.pack(padx = 5, pady = 5, side = tk.TOP, fill = tk.BOTH)
+pack0(data_frame,tk.TOP)
 
 # FileMenu Dropdown
 menubar = tk.Menu(root)
@@ -43,40 +50,41 @@ fileMenu.add_command(label="Exit", command=root.quit)
 fileMenu.add_command(label="Open Folder", command=lambda: navigator.openFolder(UpdateGraph))
 menubar.add_cascade(label="File", menu=fileMenu)
 
-# Header with info about the file
-tk.Label(data_frame, text = "What is the DELTA-MAG of ±2 neighbors on the CCD").pack(padx = 5, pady = 5, side = tk.LEFT)
-tk.Label(data_frame, text = "Target metadata:\nMAG, MAG_TYPE, target name,\nE(B-V)_gal").pack(padx = 5, pady = 5,side = tk.RIGHT)
+# Header with info about the filetempLabel(f,t,s)
+tempLabel(data_frame,"What is the DELTA-MAG of ±2 neighbors on the CCD",tk.LEFT)
+tempLabel(data_frame,"Target metadata:\nMAG, MAG_TYPE, target name,\nE(B-V)_gal",tk.RIGHT)
 
 # The graphs
 vis_frame = tk.Frame(root, bg="skyblue")
-vis_frame.pack(padx = 5, pady = 5, side = tk.TOP, fill = tk.BOTH, expand = True)
+pack2(vis_frame, tk.TOP)
 spectrum_frame = tk.Frame(vis_frame, bg = "orange")
-spectrum_frame.pack(side = tk.LEFT, fill = tk.BOTH, expand = True)
+pack2(spectrum_frame, tk.LEFT)
 canvas = CreateCanvas(bigFig, spectrum_frame)
 
 colors_frame = tk.Frame(spectrum_frame, bg = "skyblue")
-colors_frame.pack(side = tk.BOTTOM, fill = tk.BOTH, expand = True)
+pack2(colors_frame, tk.BOTTOM)
 redCanvas   = CreateCanvas(redFig,colors_frame, tk.RIGHT)
 greenCanvas = CreateCanvas(greenFig,colors_frame, tk.RIGHT)
 blueCanvas  = CreateCanvas(blueFig,colors_frame, tk.RIGHT)
 
-redshift_slide = tk.Button(spectrum_frame, text="Redshift slide, template drop-down").pack(padx = 5, pady = 5,side = tk.BOTTOM)
+tempButton(spectrum_frame,"Redshift slide, template drop-down")
 
 # Sidepanel with buttons
 spectrum_buttons = tk.Frame(vis_frame, bg = "yellow")
 spectrum_buttons.pack(side = tk.RIGHT, fill = tk.BOTH)
-show_stack = tk.Button(spectrum_buttons, text = "SHOW spectra of STACK").pack(padx = 5, pady = 5, side = tk.TOP, fill = tk.BOTH)
-show_sn = tk.Button(spectrum_buttons, text = "Show S/N spec").pack(padx = 5, pady = 5, side = tk.TOP, fill = tk.BOTH)
-show_skyview = tk.Button(spectrum_buttons, text = "Button to grab: Image cutout (DSS) 100\"x100\"", command = lambda: LoadPicture(directory, files, cursor)).pack(padx=5, pady=5,side=tk.BOTTOM, fill=tk.BOTH)
+tempButton(spectrum_buttons,"SHOW spectra of STACK")
+tempButton(spectrum_buttons,"Show S/N spec")
+tempButton(spectrum_buttons,"Button to grab: Image cutout (DSS) 100\"x100\"")
 
 # Navigation buttons in the bottom
 opts_frame = tk.Frame(root, bg="limegreen")
-opts_frame.pack(padx=5, pady=5, side=tk.BOTTOM, fill=tk.BOTH)
-tk.Button(opts_frame, text="Go Back", command = lambda: NavBtn(navigator, UpdateGraph, msg="Go Back",delta=-1)).pack(padx=5, pady=5,side=tk.LEFT, fill=tk.BOTH)
-tk.Button(opts_frame, text="Yes", command = lambda: NavBtn(navigator, UpdateGraph, msg="Yes",delta=1)).pack(padx=5, pady=5,side=tk.LEFT, fill=tk.BOTH)
-tk.Button(opts_frame, text="No", command = lambda: NavBtn(navigator, UpdateGraph, msg="No",delta=1)).pack(padx=5, pady=5,side=tk.LEFT, fill=tk.BOTH)
-tk.Button(opts_frame, text="Not sure", command = lambda: NavBtn(navigator, UpdateGraph, msg="Not sure",delta=1)).pack(padx=5, pady=5,side=tk.LEFT, fill=tk.BOTH)
-tk.Label(opts_frame, text="NO, but why:\nWrong template; wrong redshift (4XP);\nwrong class (4CP);\nBad data (L1); Maybe sat.?").pack(padx=5, pady=5,side=tk.RIGHT, fill=tk.BOTH)
+pack0(opts_frame,tk.BOTTOM)
+navButton = lambda t, d: tk.Button(opts_frame, text=t, command = lambda: NavBtn(navigator, UpdateGraph, msg=t,delta=d))
+pack0(navButton("Go Back",-1),tk.LEFT)
+pack0(navButton("Yes",1),tk.LEFT)
+pack0(navButton("No",1),tk.LEFT)
+pack0(navButton("Not sure",1),tk.LEFT)
+tempLabel(data_frame,"NO, but why:\nWrong template; wrong redshift (4XP);\nwrong class (4CP);\nBad data (L1); Maybe sat.?",tk.RIGHT)
 
 navigator.openFolder(UpdateGraph)
 
