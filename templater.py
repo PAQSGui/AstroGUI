7d4f5dfdf2d44d558bf894eb2cfe6a9601dc3b22
Artemis <jebal@itu.dk>
2025-03-24 19:59:56 +0100

    galaxy experiment. Consider grabbing the parameters from the appropriate second guess in the array.

--- templater.py
+++ templater.py
@@ -53,9 +53,11 @@ class Templater:
             print(name)
             l2_product['zBestSubType']=f'new-{name}'
             wave, model = template.create_PCA_model(target,l2_product)
-
-        chi2 = l2_product['zBestChi2'] / l2_product['zBestNfree']
-        #title_str += f"\n z = {z:.5f}  Class: {name}  $\\chi^2 = {chi2:.2f}$"
+        #except ValueError:
+        #    print(l2_product['zBestPars'])
+        #    l2_product['zBestPars']=np.append(l2_product['zBestPars'],[1,1,1,1])
+        #    print(l2_product['zBestPars'])
+        #    wave, model = template.create_PCA_model(target,l2_product)
         plt.plot(wave, model, color='r', lw=1.0, alpha=0.7, label = l2_product['zBestSubType'])
 
         #set combobox text:
