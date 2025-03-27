b027403b2940728bf9e4b66fcaf1d2898de504a6
Torben <toen@itu.dk>
2025-03-27 17:32:46 +0100

    Top section of the program is now moved to file_handling and is updated on each new file

--- file_handling.py
+++ file_handling.py
@@ -1,19 +1,44 @@
 from astropy.io import fits
 
-hdul = fits.open("data/spec-2237-53828-0531.fits", ignore_missing_simple=True)
+from PySide6.QtWidgets import (
+    QApplication, 
+    QMainWindow, 
+    QPushButton,
+    QWidget,
+    QVBoxLayout,
+    QHBoxLayout,
+    QLabel,
+    QSizePolicy,
+    )
+from PySide6.QtGui import (
+    QAction,
+    Qt,
+)
 
-for k in hdul[0].header:
-    print(k)
+# Class represents the top of the program and contains the labels used to display meta information
+class TargetData:
+    def __init__(self, name=None, mag=None, mag_type=None, ebv=None):
+        self.layout = QHBoxLayout()
+        self.nbrLabel = QLabel("What is the DELTAMAG of -+ 2 neighbors on the CCD")
+        self.targetLabel = QLabel()
+        self.targetLabel.setAlignment(Qt.AlignmentFlag.AlignRight | Qt.AlignmentFlag.AlignVCenter)
+        
+        self.layout.addWidget(self.nbrLabel)
+        self.layout.addWidget(self.targetLabel)
 
-
-def getTargetData():
-    name = hdul[0].header['NAME']
+    # Is supposed to extract target data from FITS file and update the labels accordingly
+    def updateTargetData(self, path):
+        #hdu = fits.open(path, ignore_missing_simple=True)
+        #name = hdu[0].header['NAME']
     
-    # WHY IS THERE NO EBV CARD?
-    #ebv = hdul[0].header['EBV']
+        # WHY IS THERE NO EBV CARD?
+        #ebv = hdu[0].header['EBV']
 
-    # WHERE IS THE MAGNITUDE? :(
-    #mag = hdul[0].header['*MAG*']
+        # WHERE IS THE MAGNITUDE?
+        #mag = hdu[0].header['*MAG*']
 
-
-    return name
+        #hdu.close()
+        #self.nbrLabel.setText("something with deltamag of neighbors")
+        
+        # Replace 'Nones' with data extracted from FITS file
+        self.targetLabel.setText(f"Target metadata\nNAME: {None}\nMAG: {None}\nMAG TYPE: {None}\nEBV: {None}")
