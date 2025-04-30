3a3439a38b7a85f65e5f240d942f94f4fa151341
Sofus Kjær <sofkj@itu.dk>
2025-04-30 10:17:24 +0200

    full filename saved in validation.csv

--- Fitter.py
+++ Fitter.py
@@ -34,8 +34,7 @@ class Fitter:
 
     def loadDataObject(self, itempath):
         spectra = tool.SDSS_spectrum(itempath) 
-        # THISIS WHERE 'spec' gets lost
-        obj_nme = str(itempath)[-21:][:-5]
+        obj_nme = str(itempath)[-25:][:-5]
         l2 = self.preProcess.getFitting(obj_nme)
         return DataObject(obj_nme, spectra, l2, itempath)  
 
